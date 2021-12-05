6f0fe5555541dad2ccd83041974f3933f903f31b
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2021-12-05 18:34:07 +0900

    just clear (no systemized)

--- The_Labyrinth.py
+++ The_Labyrinth.py
@@ -10,6 +10,7 @@ import math
 r, c, a = [int(i) for i in input().split()]
 print(f"row, column, alarm = {r}, {c}, {a}", file=sys.stderr)
 
+CONTROL = False
 # game loop
 while True:
     # kr: row where Rick is located.
@@ -34,4 +35,12 @@ while True:
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
     # Rick's next move (UP DOWN LEFT or RIGHT).
-    print("RIGHT")
+    if (maze[kr][kc + 1] == "." or maze[kr][kc + 1] == "C") and CONTROL == False:
+        print("RIGHT")
+
+    if maze[kr][kc] == "C":
+        CONTROL = True
+        print("LEFT")
+
+    if CONTROL == True:
+        print("LEFT")
